a488f4e638b90d2166d2027dcff62206988b3a14
Mark Macneil <mark.macneil@t4g.com>
2017-04-18 21:54:35 -0300

    fixed .net project name case

--- src/dotnetGigs/Startup.cs
+++ src/dotnetGigs/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace dotnetGigs
+namespace DotNetGigs
 {
     public class Startup
     {
